99a25830c3a8ab20d0b1c7bae804f5193578139a
bobiechen <bobiechen@gmail.com>
2015-01-05 21:36:30 -0800

    Initial Commit

---
+++ SignalCast/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SignalCast
+//
+//  Created by bobiechen on 1/5/15.
+//  Copyright (c) 2015 bobiechen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SignalCast/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SignalCast
+//
+//  Created by bobiechen on 1/5/15.
+//  Copyright (c) 2015 bobiechen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
